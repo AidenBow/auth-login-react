54c04aac84b32378eb64b80df4c45b63d93e3712
AidenBow <AidenBow@users.noreply.github.com>
2019-12-06 17:37:42 -0800

    updated serialize-javascript

--- application/src/App.js
+++ application/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome to FarmerLand!</h1>
         <SignInForm />
       </header>
     </div>
