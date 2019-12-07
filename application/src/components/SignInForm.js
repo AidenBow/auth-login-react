a0ced2f9c313875846baa64458bc9fee5c57bec4
AidenBow <AidenBow@users.noreply.github.com>
2019-12-06 19:15:09 -0800

    added form and change/submit handler to signinform

--- application/src/components/SignInForm.js
+++ application/src/components/SignInForm.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
 
 function SignInForm() {
+  const [login, setLogin] = useState({})
+  useEffect(() => {
+
+  }, [])
+
+  const handleChanges = (e) => {
+    e.preventDefault();
+    setLogin({ ...login, [e.target.name ] : e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(login)
+    axios()
+      .post("localhost:4000/api/login", login)
+      .then(res => {
+        console.log(res)
+        //props.history.push('/profile');
+      })
+    setLogin({username: "", password: ""})
+  }
+
   return (
     <div>
-      <form>
-        <input />
-      </form>
+      <form onSubmit={handleSubmit}>
+        <input 
+            type="text"
+            name="username"
+            placeholder="username"
+            onChange={handleChanges}
+            // value={userLogin.username}
+        />
+        <input 
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={handleChanges}
+            // value={userLogin.password}
+        />
+        <button type="submit" >Let's Go!</button>  
+      </form> 
     </div>
   );
 }
