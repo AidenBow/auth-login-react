42799931c016b91ee25baab473138d33a9ff9413
AidenBow <AidenBow@users.noreply.github.com>
2019-12-06 19:52:57 -0800

    created userPage and its route

--- application/src/App.js
+++ application/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Route} from "react-router-dom"
 import SignInForm from "./components/SignInForm"
+import UserPage from "./components/UserPage"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Route exact path="/" component={SignInForm}/>
+        <Route exact path="/profile" component={UserPage}/>
       </header>
     </div>
   );
---
+++ application/src/components/UserPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function UserPage() {
+
+  return(
+    <div>
+      <h1>you did it!</h1>
+    </div>
+  )
+}
+
+export default UserPage
